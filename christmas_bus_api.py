23be886492f82ea1fd18fc2a99e1801a9f7c1dd1
tysnxu <tysn.xu@gmail.com>
2023-12-16 13:14:22 +1100

    Initialize Project

---
+++ christmas_bus_api.py
@@ -0,0 +1,28 @@
+import requests
+from protobuf_to_dict import protobuf_to_dict
+
+import tfnsw_gtfs_realtime_pb2
+import tfnsw_gtfs_realtime_pb2 as transit_realtime
+
+with open("apiKey", "r") as f:
+    apiKey = f.read()
+
+url = "https://api.transport.nsw.gov.au/v1/gtfs/vehiclepos/buses"
+headers = {"Authorization": f"apikey {apiKey}"}
+
+req = requests.get(url, headers=headers)
+
+feed = tfnsw_gtfs_realtime_pb2.FeedMessage()
+feed.ParseFromString(req.content)
+
+matching_counter = 0
+
+for entity in feed.entity:
+    vehicle = entity.vehicle.vehicle
+
+    if vehicle.Extensions[transit_realtime.tfnsw_vehicle_descriptor].special_vehicle_attributes == 4:
+        print(f'Name: {entity.id.split("_")[-2]}, position: {entity.vehicle.position.latitude}, {entity.vehicle.position.longitude}')
+
+        matching_counter += 1
+
+print(f"Found {matching_counter} out of {len(feed.entity)} vehicles")
